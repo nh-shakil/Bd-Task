5dcbdf9d1d3df34c500e4765a20ef26b5e1b6f45
Risad212 <hafez.risad@gmail.com>
2022-09-05 16:44:39 +0600

    glance section done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,12 @@
-//Get the button
+// scroll effect 
+// window.onscroll = function() {scrollFunction()};
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-let navbarWrapper = document.querySelector('.navbar-wrapper')
-function scrollFunction() {
-  if (document.documentElement.scrollTop > 50) {
-      navbarWrapper.classList.add('sticky bg-white end-0 navbar_fixed position-fixed start-0 top-0')
-  } else {
-    navbarWrapper.classList.remove('sticky')
-  }
-}
+// let navbarWrapper = document.querySelector('.navbar-wrapper')
+// function scrollFunction() {
+//   if (document.documentElement.scrollTop > 50) {
+//       navbarWrapper.classList.add('sticky')
+//   } else {
+//     navbarWrapper.classList.remove('sticky')
+//   }
+// }
 
