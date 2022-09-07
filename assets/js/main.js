a15af908bff08554dbb0db5ab0c334e88898b9fe
Risad212 <hafez.risad@gmail.com>
2022-09-07 09:27:41 +0600

    fixen bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 // scroll effect 
-// window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()};
 
-// let navbarWrapper = document.querySelector('.navbar-wrapper')
-// function scrollFunction() {
-//   if (document.documentElement.scrollTop > 50) {
-//       navbarWrapper.classList.add('sticky')
-//   } else {
-//     navbarWrapper.classList.remove('sticky')
-//   }
-// }
+let navbarWrapper = document.querySelector('.navbar-wrapper')
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 50) {
+      navbarWrapper.classList.add('sticky')
+  } else {
+    navbarWrapper.classList.remove('sticky')
+  }
+}
 
